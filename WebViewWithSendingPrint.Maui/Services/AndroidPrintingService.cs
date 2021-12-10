6a474b6aee9a983f312707d8c7154a79ef3976b5
Daniel <daniel@bpapos.com>
2021-12-10 12:39:49 -0700

    Add Android printing code taken from this YouTube video: https://youtu.be/f58JxtDfTq4

--- WebViewWithSendingPrint.Maui/Services/AndroidPrintingService.cs
+++ WebViewWithSendingPrint.Maui/Services/AndroidPrintingService.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Android.Bluetooth;
+using Java.Util;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,15 +12,50 @@ namespace WebViewWithSendingPrint.Maui.Services
     public class AndroidPrintingService : IPrintingService
     {
         private readonly IConfiguration _configuration;
+        private readonly ILogger<AndroidPrintingService> _logger;
 
-        public AndroidPrintingService(IConfiguration configuration)
+        public AndroidPrintingService(IConfiguration configuration, ILogger<AndroidPrintingService> logger)
         {
             _configuration = configuration;
+            _logger = logger;
         }
 
-        public void Print(string printString)
+        /// <summary>
+        /// From
+        /// https://youtu.be/f58JxtDfTq4
+        /// </summary>
+        /// <param name="printString"></param>
+        public async void Print(string printString)
         {
-            throw new NotImplementedException();
+            try
+            {
+                BluetoothAdapter bluetoothAdapter = BluetoothAdapter.DefaultAdapter;
+                BluetoothDevice bluetoothDevice = (from bondedDevice in bluetoothAdapter?.BondedDevices
+                                                   where bondedDevice?.Name == _configuration["PrinterName"]
+                                                   select bondedDevice).FirstOrDefault();
+
+                if (bluetoothDevice == null)
+                {
+                    bluetoothDevice = bluetoothAdapter?.BondedDevices.FirstOrDefault();
+                }
+
+                if (bluetoothDevice == null)
+                {
+                    throw new NullReferenceException("No bluetooth devices were found");
+                }
+
+                await Task.Delay(1000);
+                BluetoothSocket bluetoothSocket = bluetoothDevice.CreateRfcommSocketToServiceRecord(UUID.FromString("00001101-0000-1000-8000-00805F9B34FB"));
+                bluetoothSocket?.Connect();
+                byte[] buffer = Encoding.UTF8.GetBytes(printString);
+                bluetoothSocket?.OutputStream.Write(buffer, 0, buffer.Length);
+                bluetoothSocket?.Close();
+
+            }
+            catch (Exception exception)
+            {
+                _logger.LogError(exception.ToString(), exception);
+            }
         }
     }
 }
